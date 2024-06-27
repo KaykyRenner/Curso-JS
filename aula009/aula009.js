9dc65f06bad76fbc94a98151e446bfe2689df73a
KaykyRenner <kayky.renner.sg@gmail.com>
2024-06-27 02:23:53 -0300

    a

---
+++ aula009/aula009.js
@@ -0,0 +1,7 @@
+let NomeAluno; // undefined = não aponta local nenhum da memória
+const SobreNomeAluno = null; // nulo = não aponta local nenhum da memória (usável)
+const Aprovado = true; //Boolean = True, false (valor lógico)
+console.log(typeof Aprovado)
+const a =[1,2]
+const b = a
+// tipos de dados primitivos / String, number, boolean, undefined, null
---
+++ aula010/aula010.js
@@ -0,0 +1,14 @@
+/*operadores Aritméticos
++
+-
+/
+*
+** "potenciação"
+% "resto de divisão"
+*/
+const a = 15;
+const b = 7;
+console.log(a % b)
+let contador = 1;
+contador ++;
+console.log(contador)
