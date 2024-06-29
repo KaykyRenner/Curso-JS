d9f9321bcb2930b8f74583b3a98521bb75860663
KaykyRenner <kayky.renner.sg@gmail.com>
2024-06-29 18:18:47 -0300

    voltei 29/06/2024

--- aula011/aula011.js
+++ aula011/aula011.js
@@ -1,1 +1,7 @@
-alert('meu ovo é grande')
+let num1 = prompt('digite um número');
+let num2 = prompt('digite mais um número');
+num1 = Number(num1);
+num2 = Number(num2);
+
+
+alert(`o resultado foi ${num1+num2}`);
---
+++ aula012/aula012.js
@@ -0,0 +1,7 @@
+let varA = 'A'; //B
+let varB = 'B'; //C
+let varC = 'C'; //A
+const varA1 = varB
+varB=varC
+varC = varA
+console.log(varA1, varB, varC)
