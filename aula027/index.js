a9d13eab3d6e6814952dcf1cd9d686720a919541
KaykyRenner <kayky.renner.sg@gmail.com>
2024-07-07 18:05:03 -0300

    07/07

--- aula027/index.js
+++ aula027/index.js
@@ -1,2 +1,22 @@
-const data = new Date(2024, 6, 6, 14,28,60)
-console.log(data.toString())
+//const UmaHora = 60*60*1*1000//formula para adicionar uma hora na data
+//const data = new Date(0+UmaHora)// adcionando uma hora na data
+//const data = new Date()
+//console.log('dia', data.getMonth()) 
+const data = new Date()
+function zeroEsquerd(num){
+    return num >= 10 ? num : `0${num}`;
+}
+function formataData(data){
+    const dia = zeroEsquerd(data.getDate())
+    const mes = zeroEsquerd(data.getMonth()+1)//adcionando+1 pq ele começa do mes0
+    const ano = zeroEsquerd(data.getFullYear())
+    const hora = zeroEsquerd(data.getHours())
+    const min = zeroEsquerd(data.getMinutes())
+    const seg = zeroEsquerd(data.getSeconds())
+    const ms = zeroEsquerd(data.getMilliseconds())
+    const diaSemana = zeroEsquerd(data.getDay())//0 é domingo e 6 é sábado
+
+    return `${dia}/${mes}/${ano} ${hora}:${min}:${seg}:${ms}:${diaSemana}`
+}
+const dataBrasil = formataData(data)
+console.log(dataBrasil)
---
+++ aula028/aula028.js
@@ -0,0 +1,23 @@
+const data = new Date('1987-04-22 00:00:00')
+const diaSemana = (data.getDay())
+let DiaSemanaTexto
+//if (diaSemana == 0){
+//    DiaSemanaTexto = 'domingo'
+//} else if(diaSemana == 1){
+//    DiaSemanaTexto = 'segunda'
+//}else if(diaSemana == 2){
+//    DiaSemanaTexto = 'terça'
+//}else if(diaSemana == 3){
+//    DiaSemanaTexto = 'quarta'
+//}else if(diaSemana == 4){
+//    DiaSemanaTexto = 'quinta'
+//}else if(diaSemana == 5){
+//    DiaSemanaTexto = 'sexta'
+//}else if(diaSemana == 6){
+//    DiaSemanaTexto = 'sábado'
+//} else {
+//    DiaSemanaTexto ='dia inválido, tente novamente'
+//}
+
+
+console.log(DiaSemanaTexto, diaSemana)
